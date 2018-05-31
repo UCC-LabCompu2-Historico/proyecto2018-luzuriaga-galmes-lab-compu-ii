0bec1a5bb0528943da3ff5797b2b8b4fa6b455ff
franciscogalmes <franciscogalmes@gmail.com>
2018-05-31 10:13:43 -0300

    avance javaascript

---
+++ functios.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+function mover() {
+    var ctx=document.getElementById('myCanvas').getContext('2d');
+    var image = new Image ();
+    image.src= "autoprueb.png";
+    ctx.drawImage(image, 0, 0);
+    }
+
+
