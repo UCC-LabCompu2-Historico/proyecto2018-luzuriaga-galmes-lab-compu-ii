e2d8fdae532f818ea1e592912e896e2039e139a0
franciscogalmes <franciscogalmes@gmail.com>
2018-06-06 14:04:10 -0300

    avance redimension

--- functios.js
+++ functios.js
@@ -6,8 +6,9 @@
 function mover() {
     var ctx=document.getElementById('myCanvas').getContext('2d');
     var image = new Image ();
-    image.src= "autoprueb.png";
-    ctx.drawImage(image, 0, 0);
+    image.src= "autoprueb.png" ;
+    ctx.drawImage(image,0,0,100,100,);
+    
     }
 
 
